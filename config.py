0536d8bdc51c0d0a428b51a2c2735f93a2e3324d
starsharp06sharp <leolzheng@outlook.com>
2021-08-11 15:09:54 +0800

    init config

---
+++ config.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+modems = [
+    {
+        'address': ('localhost', 9999),
+        'phone': '5775577',
+        'bps': 36600, # To be implement later
+    },
+    {
+        'address': ('localhost', 8888),
+        'phone': '7557755',
+        'bps': 36600, # To be implement later
+    },
+]
