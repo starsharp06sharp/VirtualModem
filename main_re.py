e62d526f2c8bf43de030a22340adfd17c5b009d5
starsharp06sharp <leolzheng@outlook.com>
2021-08-12 20:31:33 +0800

    finally works

--- main_re.py
+++ main_re.py
@@ -1,99 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from logging import Logger
 import selectors
 import socket
 import time
-from collections import defaultdict
-from enum import Enum
 import config
+from common import *
 
 
 sel = selectors.DefaultSelector()
 modems = []
 phone2modem = {}
-
-
-class Mode(Enum):
-    CMD = 0
-    DATA = 1
-
-
-class RespMode(Enum):
-    ECHO = -1
-    CODE = 0
-    MSG = 1
-
-
-class VConnState(Enum):
-    CONNECTING = 0
-    CONNECTED = 1
-    CLOSED = 2
-
-
-code_msg_dict = defaultdict(lambda: b'UNKNOWN RETCODE', {
-    0: b'OK',
-    1: b'RING',
-    3: b'NO CARRIER',
-    4: b'ERROR',
-    5: b'CONNECT 1200',
-    6: b'NO DIALTONE',
-    7: b'BUSY',
-    8: b'NO ANSWER',
-    9: b'CONNECT 0600',
-    10: b'CONNECT 2400',
-    11: b'CONNECT 4800',
-    12: b'CONNECT 9600',
-    13: b'CONNECT 7200',
-    14: b'CONNECT 12000',
-    15: b'CONNECT 14400',
-    16: b'CONNECT 19200',
-    17: b'CONNECT 38400',
-    18: b'CONNECT 57600',
-    19: b'CONNECT 115200',
-    22: b'CONNECT 75TX/1200RX',
-    23: b'CONNECT 1200TX/75RX',
-    24: b'DELAYED',
-    32: b'BLACKLISTED',
-    33: b'FAX',
-    35: b'DATA',
-    40: b'CARRIER 300',
-    44: b'CARRIER 1200/75',
-    45: b'CARRIER 75/1200',
-    46: b'CARRIER 1200',
-    47: b'CARRIER 2400',
-    48: b'CARRIER 4800',
-    49: b'CARRIER 7200',
-    50: b'CARRIER 9600',
-    51: b'CARRIER 12000',
-    52: b'CARRIER 14400',
-    53: b'CARRIER 16800',
-    54: b'CARRIER 19200',
-    55: b'CARRIER 21600',
-    56: b'CARRIER 24000',
-    57: b'CARRIER 26400',
-    58: b'CARRIER 28800',
-    59: b'CARRIER 31200',
-    60: b'CARRIER 33600',
-    61: b'CONNECT 16800',
-    62: b'CONNECT 21600',
-    63: b'CONNECT 24000',
-    64: b'CONNECT 26400',
-    65: b'CONNECT 28800',
-    66: b'CONNECT 33600',
-})
-
-
-def translate_resp(mode, cmd, res):
-    if isinstance(res, bytes):
-        return res
-    if isinstance(res, str):
-        return res.encode('ascii')
-    if mode == RespMode.CODE:
-        return str(res).encode('ascii')
-    if mode == RespMode.MSG:
-        global code_msg_dict
-        return code_msg_dict[res]
-    return cmd
 
 
 class VirtualConnection(object):
@@ -119,6 +36,7 @@ class VirtualConnection(object):
                 if not self.data[i]:
                     return b''
                 data = self.data[i]
+                print(f'{cur_modem.id}<{self.modems[(i+1)%2].id}|{repr(data)}')
                 self.data[i] = b''
                 return data
         return b''
@@ -172,6 +90,7 @@ class Modem(object):
 
     def recv_from_com(self, conn):
         data = self.conn.recv(4096)
+        logger.info(f'>{self.id} {repr(data)}')
         if not data:
             self.close_conn()
             return
@@ -182,24 +101,27 @@ class Modem(object):
                 if self.virtual_conn.status == VConnState.CONNECTED:
                     self.dialing = False
                     self.mode = Mode.DATA
-                    self.conn.sendall(translate_resp(
-                        self.resp_mode, b'OK', 66))
+                    res = translate_resp(self.resp_mode, b'OK', 66) + b'\r'
+                    logger.info(f'<{self.id} {repr(res)}')
+                    self.conn.sendall(res)
                 elif self.virtual_conn.status == VConnState.CLOSED:
                     self.dialing = False
-                    self.conn.sendall(translate_resp(
-                        self.resp_mode, b'BUSY', 7))
+                    res = translate_resp(self.resp_mode, b'BUSY', 7) + b'\r'
+                    logger.info(f'<{self.id} {repr(res)}')
+                    self.conn.sendall(res)
                 else:
-                    time.sleep(0.1)
+                    time.sleep(0.3)
                     return
+
             if self.mode == Mode.DATA:
-                pindex = self.recv_buffer.find(b'+++')
-                if pindex >= 0:
-                    package = self.recv_buffer[:pindex]
-                    self.recv_buffer = self.recv_buffer[pindex+3:]
-                    self.mode = Mode.CMD
-                else:
-                    package = self.recv_buffer
+                # The escape sequence was preceded and followed by one second of silence
+                if self.recv_buffer == b'+++':
+                    print(f'{self.id}+++')
                     self.recv_buffer = b''
+                    self.mode = Mode.CMD
+                    continue
+                package = self.recv_buffer
+                self.recv_buffer = b''
                 if not package:
                     continue
                 self.virtual_conn.push_data(self, package)
@@ -215,7 +137,9 @@ class Modem(object):
                     continue
                 res = self.dispatch_command(cmd)
                 if res != -1:
-                    self.conn.sendall(res + b'\r')
+                    res += b'\r'
+                    logger.info(f'<{self.id} {repr(res)}')
+                    self.conn.sendall(res)
 
     def dispatch_command(self, cmd):
         res = 0
@@ -250,6 +174,7 @@ class Modem(object):
             if self.virtual_conn.status != VConnState.CLOSED:
                 self.virtual_conn.status = VConnState.CONNECTED
                 self.mode = Mode.DATA
+                res = 66
             else:
                 self.virtual_conn = None
                 res = 8
@@ -258,7 +183,6 @@ class Modem(object):
                 # disable in one way
                 self.virtual_conn.status = VConnState.CLOSED
                 self.virtual_conn = None
-
 
         if res != -1:
             res = translate_resp(self.resp_mode, cmd, res)
@@ -279,6 +203,7 @@ class Modem(object):
         # Calling from remote
         if not self.dialing and self.virtual_conn.status == VConnState.CONNECTING:
             print(f'>{self.id}|RING')
+            logger.info(f'<{self.id} b\'RING\\r\'')
             self.conn.sendall(b'RING\r')
             return
         # FIXME: dirty implementation, refactor it by asyncio
@@ -287,23 +212,31 @@ class Modem(object):
                 self.dialing = False
                 self.mode = Mode.DATA
                 print(f'{self.id}<|CONNECT')
-                self.conn.sendall(translate_resp(
-                    self.resp_mode, b'CONNECT', 66))
+                res = translate_resp(self.resp_mode, b'CONNECT', 66) + b'\r'
+                logger.info(f'<{self.id} {repr(res)}')
+                self.conn.sendall(res)
             elif self.virtual_conn.status == VConnState.CLOSED:
                 self.dialing = False
                 print(f'{self.id}<|BUSY')
-                self.conn.sendall(translate_resp(
-                    self.resp_mode, b'BUSY', 7))
+                res = translate_resp(self.resp_mode, b'BUSY', 7) + b'\r'
+                logger.info(f'<{self.id} {repr(res)}')
+                self.conn.sendall(res)
 
     def try_send_data(self):
         if not self.virtual_conn:
             return
         data = self.virtual_conn.fetch_data(self)
         if data:
+            logger.info(f'<{self.id} {repr(data)}')
             self.conn.sendall(data)
         # close this virtual_conn completely when half closed by remote
         if self.virtual_conn.status == VConnState.CLOSED:
             self.virtual_conn = None
+            print(f'{self.id}<|NO CARRIER')
+            res = translate_resp(self.resp_mode, b'NO CARRIER', 3)+b'\r'
+            logger.info(f'<{self.id} {repr(res)}')
+            self.conn.sendall(res)
+            self.mode = Mode.CMD
 
 
 def create_accept_func(m):
