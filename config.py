cda812bb3fbe8c99e7593fc08859c1a4e161c4c8
starshar06sharp <leolzheng@outlook.com>
2021-08-14 23:51:47 +0800

    fix bps error

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ modems = [
     {
         'address': ('localhost', 9999),
         'phone': '5775577',
-        'bps': 36600,  # To be implement later
+        'bps': 33600,  # To be implement later
     },
     {
         'address': ('localhost', 8888),
         'phone': '7557755',
-        'bps': 36600,  # To be implement later
+        'bps': 33600,  # To be implement later
     },
 ]
